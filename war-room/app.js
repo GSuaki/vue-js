9ffc922fc3250bfe570ce317ed752a6c3e631145
gsuaki <gsuaki@hotmail.com>
2017-10-31 23:26:30 +0000

    power bar

--- war-room/app.js
+++ war-room/app.js
@@ -3,51 +3,75 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
+    powerBar: 0,
     gamingIsRunning: false
   },
   methods: {
     startGame: function() {
       this.gamingIsRunning = true;
       this.monsterHealth = this.playerHealth = 100;
+      this.powerBar = 0;
     },
     attack: function() {
 
-      var max = 10;
-      var min = 3;
-
-      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-      
+      var damage = this.calculateDamage(3, 10);
       this.monsterHealth -= damage;
+      this.powerBar += damage;
 
-      if (this.monsterHealth <= 0) {
-        alert('You won!');
-        this.gamingIsRunning = false;
-        return;
-      }
+      if (this.checkWin())  return;
 
-      max = 12;
-      min = 5;
-
-      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-      
-      this.playerHealth -= damage;
-
-      if (this.playerHealth <= 0) {
-        alert('You lost!');
-        this.gamingIsRunning = false;
-      }
+      this.monsterAttacks();
     },
     specialAttack: function() {
       
-      this.monsterHealth -= 15;
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      this.powerBar = 0;
+
+      if (this.checkWin())  return;
+
+      this.monsterAttacks();
     },
     heal: function() {
 
-      this.playerHealth += 10;
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+
+      this.monsterAttacks();
     },
     giveUp: function() {
-
       this.gamingIsRunning = false;
+    },
+    monsterAttacks: function() {
+      this.playerHealth -= this.calculateDamage(5, 15);
+      this.checkWin()
+    },
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function() {
+
+      var vm = this;
+
+      var restart = function(word) {
+        if (confirm('You ' + word + '! Restart game?')) {
+          vm.startGame()
+        } else {
+          vm.gamingIsRunning = false;
+        }
+      }
+
+      if (this.monsterHealth <= 0) {
+        restart('won');
+        return true;
+      } else if (this.playerHealth <= 0) {
+        restart('lost');
+        return true;
+      }
+
+      return false;
     }
   }
 });
